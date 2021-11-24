02f1ac00da59460687bc41a31c8e9643a617900c
PaulaZP <mzp1405@gmail.com>
2021-11-24 17:35:42 -0600

    Agrego tarea 4

--- src/components/AlertTag.js
+++ src/components/AlertTag.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom'
 
 const alertContainer = document.querySelector("#alert-root")
 
-const AlertTag = ({ message, type, isOpened, onClose, success}) => {
+const AlertTag = ({ message, type, isOpened}) => {
 
     if (!isOpened) return null
     return ReactDOM.createPortal(
             <div className={`alert ${type}`}>
+                <div className = "closeWindow" onClick = {(e) => {e.target.parentElement.style.display = 'none'}}>X</div>
                 <span>{message}</span>
             </div>,
         alertContainer
